000d8617e68e62a07ef59277d62e905cfdfaf139
Bulandy <18844116763@163.com>
2023-06-12 09:22:56 +0000

    add the core interface of stargate

--- contracts/vault.sol
+++ contracts/vault.sol
@@ -16,8 +16,42 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /**
 Farm type interface
  */
+
+    interface StargateFi{
+
+        //0x45a01e4e04f14f7a4a6702c74187c5f6222033cd :: Stargate Router
+        function addLiquidity(
+            uint256 _poolId, //USDT : 2 
+            uint256 _amountLD, //1000000
+            address _to //address(this)
+        ) external;
+        function instantRedeemLocal(
+            uint16 _srcPoolId,//USDT : 2
+            uint256 _amountLP,//balanceOf(address(this))
+            address _to //address(this)
+        ) external;
+
+
+        //0x8731d54e9d02c286767d56ac03e8037c07e01e98
+        function deposit(
+            uint256 _pid, //USDT : 2 
+            uint256 _amount //1000000
+            ) external;
+        function withdraw(
+            uint256 _pid, 
+            uint256 _amount
+            ) external;
+    }
+
     interface BeefyFarm{
-        
+        /**
+         * 0x1c480521100c962f7da106839a5a504b5a7457a1 :: Stargate USDT
+         * 
+         */
+        function deposit(uint _amount) external;
+        function depositAll() external;
+        function withdrawAll() external;
+        function withdraw(uint256 _shares) external;
     }
 
 /**
@@ -32,6 +66,17 @@ Swap type interface
         function get_dy_underlying (uint128 i,uint128 j,uint256 dx) external view returns (uint256);
         function exchange (uint128 i,uint128 j,uint256 dx,uint256 mini_dy) external ;
         function exchange_underlying (uint128 i,uint128 j,uint256 dx,uint256 mini_dy) external ;
+    }
+
+    //Stable Coin Swap
+    interface Isynapase {
+        function swap(
+            uint8 tokenIndexFrom,
+            uint8 tokenIndexTo,
+            uint256 dx,
+            uint256 minDy,
+            uint256 deadline
+        ) external returns (uint256);
     }
 
 /**
