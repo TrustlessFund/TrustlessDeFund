7b78406a81b29bf969eab8f742a026a88d4f4e10
Bulandy <18844116763@163.com>
2023-06-16 05:58:46 +0000

    checking and onboarding leverage trading interface ;

--- contracts/vault.sol
+++ contracts/vault.sol
@@ -83,8 +83,36 @@ Swap type interface
 Leverage type interface
  */
     interface GMX{
+        //GMX V1 Interface
+    function createDecreasePosition(
+        address[] memory _path,
+        address _indexToken,
+        uint256 _collateralDelta,
+        uint256 _sizeDelta,
+        bool _isLong,
+        address _receiver,
+        uint256 _acceptablePrice,
+        uint256 _minOut,
+        uint256 _executionFee,
+        bool _withdrawETH
+    ) external payable ;
 
+    function createIncreasePosition(
+        address[] memory _path,
+        address _indexToken,
+        uint256 _amountIn,
+        uint256 _minOut,
+        uint256 _sizeDelta,
+        bool _isLong,
+        uint256 _acceptablePrice,
+        uint256 _executionFee,
+        bytes32 _referralCode
+    ) external payable;
+    function executeIncreasePosition(bytes32 _key, address payable _executionFeeReceiver) external;
+    function executeDecreasePosition(bytes32 _key, address payable _executionFeeReceiver) external;
     }
+
+
 
     interface GNS{
 
